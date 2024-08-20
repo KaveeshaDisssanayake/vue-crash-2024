ae6addfbde6a094cff64728092b8faa93d214996
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-08-20 16:24:35 +0530

    Job Cards 1

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Navbar from './components/Navbar.vue';
 import Hero from './components/Hero.vue';
 import HomeCards from './components/HomeCards.vue';
+import JobListnings from './components/JobListnings.vue';
 </script>
 
 <template>
  <Navbar/>
  <Hero title="Discover the Perfect Vue Opportunity for You" subtitle = " Secure the Vue role that aligns with your expertise and career goals."  />
  <HomeCards/>
+ <JobListnings/>
 </template>
---
+++ src/components/JobListnings.vue
@@ -0,0 +1,21 @@
+<script setup>
+import jobData from '@/jobs.json';
+import { ref } from 'vue';
+
+const jobs = ref(jobData);
+
+
+</script>
+
+<template>
+    <section class="bg-blue-50 px-4 py-10 ">
+     <div class="container-xl lg:container m-auto ">
+        <h2 class="text-3-xl font-bold text-green-500 mb-6 text-center">
+            Browse Jobs
+        </h2>
+     </div>
+    </section>
+  <div v-for="job in jobs" :key="job.id">
+    {{ job.title }}
+  </div>
+</template>
