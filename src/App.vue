568f3406765cea27378a9afd59156b648782735e
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-08-20 15:01:55 +0530

    HomeCards & Card Container Component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
 import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
 </script>
 
 <template>
  <Navbar/>
  <Hero title="Discover the Perfect Vue Opportunity for You" subtitle = " Secure the Vue role that aligns with your expertise and career goals."  />
+ <HomeCards/>
 </template>
